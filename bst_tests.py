4b06ade90c6322285f346b25905c0333e454a09d
Jonah Chan <123jonahchan@gmail.com>
2025-10-31 08:35:17 -0700

    initial commit for bst_tests.py

---
+++ bst_tests.py
@@ -0,0 +1,15 @@
+import sys
+import unittest
+from typing import *
+from dataclasses import dataclass
+
+sys.setrecursionlimit(10**6)
+from bst import *
+
+
+class BSTTests(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
